f1f879eaab41f56d36356b7d947e8a8e6202f417
javiermartingonzalez@gmail.com <javiermartingonzalez@gmail.com@296963eb-753c-cbb1-a961-c059c1fa8342>
2014-05-03 21:22:56 +0000

    Grabando! Exitos Totales.. Guarda la cursada.. tiene bugs como el año pero anda bien!

--- sundae/src/main/java/com/uai/service/MateriaService.java
+++ sundae/src/main/java/com/uai/service/MateriaService.java
@@ -1,12 +1,16 @@
 package com.uai.service;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.uai.dao.IMateriaDAO;
+import com.uai.model.Cursada;
 import com.uai.model.Materia;
+import com.uai.model.Tipo_Cursada;
 import com.uai.model.Usuario;
 
 @Named("materiaService")
@@ -22,5 +26,24 @@ public class MateriaService implements IMateriaService{
 
 	public List<Materia> getAllMaterias(Usuario usr) {
 		return materiaDAO.getAllMaterias(usr);
+	}
+
+	@SuppressWarnings("deprecation")
+	public void setCursada(List<Materia> cursadaActual, Usuario usr, Tipo_Cursada tipoCursada) {
+		List<Cursada> lista = new ArrayList<Cursada>();
+		for (Materia materia : cursadaActual) {
+			Cursada curs = new Cursada();
+			curs.setMateria(materia);
+			curs.setTipo_cursada(tipoCursada);
+			curs.setUsuario(usr);
+			curs.setFechaInicioCursada(new Date(2014,04,01));
+			curs.setFechaFinCursada(new Date(2014,07,01));
+			lista.add(curs);
+		}
+		materiaDAO.setCursada(lista);
+	}
+
+	public List<Tipo_Cursada> getTiposCursadas() {
+		return materiaDAO.getTiposCursadas();
 	}
 }
